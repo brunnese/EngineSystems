3dd43476cd3251027a3d35c1aa0a32e9e8fbf39a
dograf <42509345+dograf@users.noreply.github.com>
2021-10-22 15:07:39 +0200

    Add files via upload

---
+++ Dominik/Par.m
@@ -0,0 +1,33 @@
+
+
+% Thermodynamics
+par.R = 287;
+
+% Throttle
+par.alpha0 = 3*1e-6;
+par.alpha1 = 6*1e-6;
+
+% Intake Manifold
+par.Vm = 7*1e-3;
+
+% Engine mass flow
+par.Vd = 2.48*1e-3;
+par.Vc = 2.48*1e-4;
+par.sigma0 = 14.7;
+par.kappa = 1.35
+par.gamma0 = .6;
+par.gamma1 = .002;
+
+% Engine torque generation
+par.Hl = 42.5*1e6;
+par.k = 2.3345*13-4;
+par.eta0 = .3;
+par.eta1 = -3*1e-4;
+par.beta0 = 7;
+
+% Engine inertia
+par.thetaE = .2;
+
+% Load torque
+par.etaGen = .7;
+
