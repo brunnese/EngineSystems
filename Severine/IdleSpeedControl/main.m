caecad19d4cc5174cb60f598d2e4b8865012919d
brunnese <61410189+brunnese@users.noreply.github.com>
2021-10-25 12:20:23 +0200

    Create main.m

---
+++ Severine/IdleSpeedControl/main.m
@@ -0,0 +1,13 @@
+clc 
+clear all
+
+run Par;
+timespan = [0 10];
+du = 25;
+ualpha = 50;
+theta_m = 293;
+theta_a = 293;
+p_a = 10^5;
+p_e = 1.2*10^5;
+
+simout = sim('ISC_V1', timespan, par.simopt);
