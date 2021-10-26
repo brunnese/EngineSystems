aa2fa8885687fcad79b28c88e5f52835573837f9
dograf <42509345+dograf@users.noreply.github.com>
2021-10-26 11:13:42 +0200

    Add files via upload

--- Dominik/Par.m
+++ Dominik/Par.m
@@ -1,3 +1,6 @@
+% Simulation Options
+
+par.simopt = simset('Solver','ode1','FixedStep',1e-3,'SrcWorkspace', 'current');
 
 
 % Thermodynamics
@@ -17,17 +20,21 @@ par.sigma0 = 14.7;
 par.kappa = 1.35
 par.gamma0 = .6;
 par.gamma1 = .002;
+par.mdot_beta0 = 0.01;
 
 % Engine torque generation
 par.Hl = 42.5*1e6;
-par.k = 2.3345*13-4;
+par.k = 2.3345*1e-4;
 par.eta0 = .3;
 par.eta1 = -3*1e-4;
 par.beta0 = 7;
+par.p_me0f = par.beta0*4*pi/par.Vd;
+par.lambda = 1;
 
 % Engine inertia
 par.thetaE = .2;
+par.omegae0 = 100;
 
 % Load torque
 par.etaGen = .7;
-
+par.Pl = 1000;
--- Dominik/main.m
+++ Dominik/main.m
@@ -1,1 +1,29 @@
-jfgndfkjgnd
+clc 
+clear all
+
+run Par;
+timespan = [0 10];
+du = 25;
+ualpha = 5;
+theta_m = 295;
+theta_a = 295;
+p_a = 10^5;
+p_e = 1.2*10^5;
+
+[t,x,y] = sim('ISC_V1', timespan, par.simopt);
+
+subplot(3,1,1)
+grid on, hold on
+plot(t,y(:,1)/1e5)
+xlabel('Time [s]')
+ylabel('$Pressure$ [bar]','interpreter','latex')
+subplot(3,1,2)
+grid on, hold on
+plot(t,y(:,2)*1e3)
+xlabel('Time [s]')
+ylabel('$\dot{m}_{\alpha}$ [g/s]','interpreter','latex')
+subplot(3,1,3)
+grid on, hold on
+plot(t,y(:,3)*1e3)
+xlabel('Time [s]')
+ylabel('$\dot{m}_{\beta}$ [g/s]','interpreter','latex')
